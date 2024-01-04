e389d2cd02a889a76543417ce1c57d208f337772
amandats4i <amandatsai16@gmail.com>
2024-01-04 11:56:27 -0300

    Criaçao do CRUD de Produto

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,82 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name = "tb_produto")
+
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Por gentileza, coloque um nome para o produto!")
+	private String nome;
+	
+	@NotBlank
+	private String prescricao;
+	
+	
+	@NotNull(message = "Por gentileza, insira o preço do produto!")
+	@Digits(integer = 6, fraction = 2)
+	private BigDecimal preco;
+	
+	private String foto;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPrescricao() {
+		return prescricao;
+	}
+
+	public void setPrescricao(String prescricao) {
+		this.prescricao = prescricao;
+	}
+
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+	
+}
