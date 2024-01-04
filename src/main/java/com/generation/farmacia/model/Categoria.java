b4e960f4288caeb84541ee07d31e455fa44499aa
amandats4i <amandatsai16@gmail.com>
2024-01-04 12:07:23 -0300

    Relaçao entre classes

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,11 +1,16 @@
 package com.generation.farmacia.model;
 
-import jakarta.persistence.Entity;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
@@ -21,6 +26,22 @@ public class Categoria {
 	@NotBlank(message = "O Atributo catergoria é obrigatório")
 	private String categoria;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE) 
+	@JsonIgnoreProperties("categoria")
+	
+	private List<Produto>produto;
+
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	
 
 	public Long getId() {
 		return id;
--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -2,11 +2,13 @@ package com.generation.farmacia.model;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
@@ -34,6 +36,18 @@ public class Produto {
 	private BigDecimal preco;
 	
 	private String foto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categoria categoria;
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 	
 
 	public Long getId() {
